9bb9c4c2fa72411a0a3de10eb68bfe560ecef9d1
Smadar Tsdaka <smadar@irpsystems.com>
2025-04-16 13:52:07 +0300

    Change the colors of the PropertyGrid

--- DashboardHandler/ViewModels/PropertyGridViewModel.cs
+++ DashboardHandler/ViewModels/PropertyGridViewModel.cs
@@ -1,11 +1,43 @@
 ﻿
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DashboardHandler.Models.ToolsDesign;
+using Syncfusion.Windows.PropertyGrid;
 
 namespace DashboardHandler.ViewModels
 {
     public class PropertyGridViewModel: ObservableObject
     {
-        public DesignToolBase SelectedNode { get; set; }
+		#region Properties
+
+		public DesignToolBase SelectedNode { get; set; }
+
+		#endregion Properties
+
+		#region Constructor
+
+		public PropertyGridViewModel() 
+		{
+			Grid_AutoGeneratingPropertyGridItemCommand = 
+				new RelayCommand<AutoGeneratingPropertyGridItemEventArgs>(Grid_AutoGeneratingPropertyGridItem);
+		}
+
+		#endregion Constructor
+
+		#region Methods
+
+		private void Grid_AutoGeneratingPropertyGridItem(
+			AutoGeneratingPropertyGridItemEventArgs e)
+		{
+
+		}
+
+		#endregion Methods
+
+		#region Commands
+
+		public RelayCommand<AutoGeneratingPropertyGridItemEventArgs> Grid_AutoGeneratingPropertyGridItemCommand { get; private set; }
+
+		#endregion Commands
 	}
 }
