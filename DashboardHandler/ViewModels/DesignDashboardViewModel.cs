346143ae0654cd8ec39b95c0430ca7771749d582
Smadar Tsdaka <smadar@irpsystems.com>
2025-05-26 16:30:39 +0300

    Handle delete of diagram node

--- DashboardHandler/ViewModels/DesignDashboardViewModel.cs
+++ DashboardHandler/ViewModels/DesignDashboardViewModel.cs
@@ -61,6 +61,7 @@ namespace DashboardHandler.ViewModels
 			PageSettings = new PageSettings();
 
 			ItemAddedCommand = new RelayCommand<object>(ItemAdded);
+			ItemDeletedCommand = new RelayCommand<object>(ItemDeleted);
 			Diagram_DropCommand = new RelayCommand<DragEventArgs>(Diagram_Drop);
 			ItemSelectedCommand = new RelayCommand<object>(ItemSelected);
 			SaveDashboradCommand = new RelayCommand(Save);
@@ -91,179 +92,6 @@ namespace DashboardHandler.ViewModels
 		private void GenerateDashborad()
 		{
 			_generateService.Generate(DesignDiagram);
-		}
-
-		private void AddNodesForDebug()
-		{
-			DesignToolBase designTool = new DesignToolSwitch()
-			{
-				IsChecked = true,
-				OnDescription = "ON",
-				OffDescription = "OFF",
-			};
-
-			NodeViewModel node = new NodeViewModel()
-			{
-				OffsetX = 100,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "Switch",
-
-				ContentTemplate = Application.Current.Resources["NodeSwitchTemplate"] as DataTemplate
-			};
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolComboBox()
-			{
-				//Items = new ObservableCollection<object> { "Item 1", "Item 2", "Item 3", }
-			};
-			//(designTool as DesignToolComboBox).SelectedItem =
-			//	(designTool as DesignToolComboBox).Items[1];
-
-			node = new NodeViewModel()
-			{
-				OffsetX = 200,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "ComboBox",
-
-				ContentTemplate = Application.Current.Resources["NodeComboBoxTemplate"] as DataTemplate
-			};
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolTextBox()
-			{
-				Text = "The Text!!",
-			};
-
-			node = new NodeViewModel()
-			{
-				OffsetX = 300,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "TextBox",
-
-				ContentTemplate = Application.Current.Resources["NodeTextBoxTemplate"] as DataTemplate
-			};
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolLed()
-			{
-				IsChecked = false,
-				OffColor = Brushes.Red,
-				OnColor = Brushes.Green,
-			};
-
-			node = new NodeViewModel()
-			{
-				UnitHeight = 30,
-				UnitWidth = 30,
-
-				OffsetX = 400,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "Led",
-
-				ContentTemplate = Application.Current.Resources["NodeLedTemplate"] as DataTemplate
-			};
-
-			designTool.ParentNode = node;
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolGauge()
-			{
-				
-			};
-
-			node = new NodeViewModel()
-			{
-				UnitHeight = 30,
-				UnitWidth = 30,
-
-				OffsetX = 500,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "Gauge",
-
-				ContentTemplate = Application.Current.Resources["NodeGaugeTemplate"] as DataTemplate
-			};
-
-			designTool.ParentNode = node;
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolChart()
-			{
-
-			};
-
-			node = new NodeViewModel()
-			{
-				UnitHeight = 30,
-				UnitWidth = 30,
-
-				OffsetX = 600,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "Chart",
-
-				ContentTemplate = Application.Current.Resources["NodeChartTemplate"] as DataTemplate
-			};
-
-			designTool.ParentNode = node;
-
-			Nodes.Add(node);
-
-			///////////////////////////////////////////////////////////
-
-			designTool = new DesignToolRegister()
-			{
-
-			};
-
-			node = new NodeViewModel()
-			{
-				UnitWidth = 70,
-
-				OffsetX = 700,
-				OffsetY = 100,
-				Pivot = new Point(0, 0),
-
-				Content = designTool,
-				ID = "Register",
-
-				ContentTemplate = Application.Current.Resources["NodeRegisterTemplate"] as DataTemplate
-			};
-
-			designTool.ParentNode = node;
-
-			Nodes.Add(node);
 		}
 
 		public void Save()
@@ -309,6 +137,17 @@ namespace DashboardHandler.ViewModels
 			{
 				InitNodeBySymbol(node, (itemAdded.Info as PasteCommandInfo).SourceId as string);
 			}
+		}
+
+		private void ItemDeleted(object item)
+		{
+			if (!(item is ItemDeletedEventArgs itemDeleted))
+				return;
+
+			if (!(itemDeleted.Item is NodeViewModel node))
+				return;
+
+			DesignDiagram.ToolList.Remove(node.Content as DesignToolBase);
 		}
 
 		private void InitNodeBySymbol(
@@ -536,12 +375,17 @@ namespace DashboardHandler.ViewModels
 		#region Commands
 
 		public RelayCommand<object> ItemAddedCommand { get; private set; }
+		public RelayCommand<object> ItemDeletedCommand { get; private set; }
 		public RelayCommand<DragEventArgs> Diagram_DropCommand { get; private set; }
 		public RelayCommand<object> ItemSelectedCommand { get; private set; }
 
 
 		public RelayCommand SaveDashboradCommand { get; private set; }
 		public RelayCommand GenerateDashboradCommand { get; private set; }
+
+		public RelayCommand CopyCommand { get; private set; }
+		public RelayCommand PastCommand { get; private set; }
+		public RelayCommand SaveCommand { get; private set; }
 
 		#endregion Commands
 	}
